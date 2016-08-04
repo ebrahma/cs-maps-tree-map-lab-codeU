cbf255f5160e4bed9d521503751b877a1785f780
ebrahma <ebrahma@gmail.com>
2016-08-03 23:14:16 -0500

    Done.

--- javacs-lab09/src/com/flatironschool/javacs/MyTreeMap.java
+++ javacs-lab09/src/com/flatironschool/javacs/MyTreeMap.java
@@ -1,8 +1,5 @@
-/**
- * 
- */
 package com.flatironschool.javacs;
-
+import java.util.*;
 import java.util.Collection;
 import java.util.Deque;
 import java.util.HashSet;
@@ -73,6 +70,16 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 		
 		// the actual search
         // TODO: Fill this in.
+		Node node = root;
+        while (node != null) {
+            int cmp = k.compareTo(node.key);
+            if (cmp < 0)
+                node = node.left;
+            else if (cmp > 0)
+                node = node.right;
+            else
+                return node;
+        }
         return null;
 	}
 
@@ -92,6 +99,22 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 
 	@Override
 	public boolean containsValue(Object target) {
+		return containsValueHelper(root, target);
+	}
+
+	public boolean containsValueHelper(Node node, Object target) {
+		if (node == null) {
+			return false;
+		}
+		if (equals(target, node.value)) {
+			return true;
+		}
+		if (containsValueHelper(node.left, target)) {
+			return true;
+		}
+		if (containsValueHelper(node.right, target)) {
+			return true;
+		}
 		return false;
 	}
 
@@ -117,8 +140,15 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 	@Override
 	public Set<K> keySet() {
 		Set<K> set = new LinkedHashSet<K>();
-        // TODO: Fill this in.
+		addInOrder(root, set);
 		return set;
+	}
+	
+	private void addInOrder(Node node, Set<K> set) {
+		if (node == null) return;
+		addInOrder(node.left, set);
+		set.add(node.key);
+		addInOrder(node.right, set);		
 	}
 
 	@Override
@@ -135,8 +165,31 @@ public class MyTreeMap<K, V> implements Map<K, V> {
 	}
 
 	private V putHelper(Node node, K key, V value) {
-        // TODO: Fill this in.
-        return null;
+		@SuppressWarnings("unchecked")
+		Comparable<? super K> k = (Comparable<? super K>) key;
+		int cmp = k.compareTo(node.key);
+		
+		if (cmp < 0) {
+			if (node.left == null) {
+				node.left = new Node(key, value);
+				size++;
+				return null;
+			} else {
+				return putHelper(node.left, key, value);
+			}
+		}
+		if (cmp > 0) {
+			if (node.right == null) {
+				node.right = new Node(key, value);
+				size++;
+				return null;
+			} else {
+				return putHelper(node.right, key, value);
+			}
+		}
+		V oldValue = node.value;
+		node.value = value;
+		return oldValue;
 	}
 
 	@Override
